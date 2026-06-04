cc6c99df9e88bd4d0951fed89302413e7a27ffc5
Nadine1117 <cnadinecabral@gmail.com>
2026-06-04 19:31:41 -0300

    Crear Registro de usuarios
    (se probo en postman y quedo ok)

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const sequelize = require("./config/database");
 const User = require("./models/User");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando");
