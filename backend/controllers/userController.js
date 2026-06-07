39310a9ce4e50a9832f900dda8be8eb14870dbd5
paxwi <paudobrzalovski@gmail.com>
2026-06-06 22:36:04 -0300

    Agregué login y JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,36 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   try {
     const { nombre, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({
-      nombre,
-      email,
-      password: hashedPassword,
-    });
-
+    const user = await User.create({ nombre, email, password: hashedPassword });
     res.status(201).json(user);
   } catch (error) {
-    res.status(500).json({
-      mensaje: "Error al registrar usuario",
-      error,
-    });
+    res.status(500).json({ mensaje: "Error al registrar usuario", error });
   }
 };
 
-module.exports = {
-  register,
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user)
+      return res.status(404).json({ mensaje: "Usuario no encontrado" });
+    const passwordValida = await bcrypt.compare(password, user.password);
+    if (!passwordValida)
+      return res.status(401).json({ mensaje: "Contraseña incorrecta" });
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" },
+    );
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ mensaje: "Error al iniciar sesión", error });
+  }
 };
+
+module.exports = { register, login };
